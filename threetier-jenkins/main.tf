2725eaa5b74b928a8a49f809ab8c53a1ab291b70
pranit-butale <pranitbutale@gmail.com>
2026-01-03 09:54:41 +0530

    eks cluster

--- threetier-jenkins/main.tf
+++ threetier-jenkins/main.tf
@@ -6,17 +6,21 @@ provider "aws" {
 
 ### Get Default VPC Subnets
 
-data "aws_vpc" "default" {
-  default = true
+data "aws_vpc" "default_vpc" {
+    default = true 
+  
 }
 
-data "aws_subnets" "default" {
+data "aws_subnets" "default_subnets" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+    values = [data.aws_vpc.default_vpc.id]
   }
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
-
+}
 
 ### IAM Role for EKS Cluster
 
